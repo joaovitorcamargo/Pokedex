71a2ebb7e9fd37fdc7c7221cab35150d7466ba98
joaovitorcamargo <joao.almeida@eduxe.com.br>
2021-08-20 14:05:42 -0300

    first commit

---
+++ components/Pokedex.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div v-for="pokemon in pokemons" :key="pokemon.id">
+      <img :src="maskZeros(pokemon.id, 3)" alt="" />
+      {{ pokemon.name.english }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["pokemons"],
+  data() {
+    return {
+      pokemons_id: [],
+    };
+  },
+  methods: {
+    maskZeros(value, padding) {
+      let zero = new Array(padding + 1).join("0");
+      return (
+        "https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/images/" +
+        (zero + value).slice(-padding) +
+        ".png"
+      );
+    },
+  },
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <Pokedex :pokemons="pokemons" />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      pokemons: [],
+    };
+  },
+  methods: {
+    getPokemons() {
+      this.pokemons = [];
+      fetch(
+        `https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          this.pokemons = data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    test() {
+      this.getPokemons();
+    },
+  },
+  mounted() {
+    this.getPokemons();
+  },
+};
+</script>
